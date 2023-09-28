80c1b2248f617573551db7c04478fbb4fb6e55c6
Christian_de_Winter <christian.bitacademy@gmail.com>
2023-09-28 10:26:32 +0200

    .\Login.php

---
+++ Login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Login Page</title>
+           <link rel="stylesheet" href="login.css">
+      </head>  
+      <body>  
+           <br />
+              <div class="center">
+      <h1>Login</h1>
+      <form method="post">
+        <div class="txt_field">
+          <input type="username" name="username" required>
+          <span></span>
+          <label>Gebruikersnaam</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" name="password" required>
+          <span></span>
+          <label>Wachtwoord</label> 
+        </div>
+        <div class="btn_field">
+          <button type="submit" name="login">Inloggen</button>
+
+
+      </body>  
+ </html>  
